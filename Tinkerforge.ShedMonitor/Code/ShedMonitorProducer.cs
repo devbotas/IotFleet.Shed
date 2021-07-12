e0aefa8e6204da119bd0b16d35b779e344fc03cd
devbotas <simon@devbot.lt>
2021-07-12 11:52:29 +0300

    Updating Nugets, adapting code.

--- Tinkerforge.ShedMonitor/Code/ShedMonitorProducer.cs
+++ Tinkerforge.ShedMonitor/Code/ShedMonitorProducer.cs
@@ -8,19 +8,19 @@ using Tinkerforge;
 namespace ShedMonitor {
     class ShedMonitorProducer {
         private CancellationTokenSource _globalCancellationTokenSource = new CancellationTokenSource();
-        private ResilientHomieBroker _broker = new ResilientHomieBroker();
+        private PahoHostDeviceConnection _broker = new PahoHostDeviceConnection();
 
         private HostDevice _device;
-        public HostFloatProperty Pressure;
-        public HostFloatProperty Temperature;
-        public HostFloatProperty Humidity;
-        public HostFloatProperty QualityIndex;
+        public HostNumberProperty Pressure;
+        public HostNumberProperty Temperature;
+        public HostNumberProperty Humidity;
+        public HostNumberProperty QualityIndex;
 
-        public HostFloatProperty WaterPressure;
+        public HostNumberProperty WaterPressure;
 
         private DateTime _startTime = DateTime.Now;
-        private HostFloatProperty _systemUptime;
-        private HostStringProperty _systemIpAddress;
+        private HostNumberProperty _systemUptime;
+        private HostTextProperty _systemIpAddress;
 
         public BrickletAirQuality AirQualityBricklet { get; set; }
         public BrickletIndustrialDual020mA Industrial020Bricklet { get; set; }
@@ -36,26 +36,29 @@ namespace ShedMonitor {
 
             Log.Info($"Creating Homie properties.");
             _device.UpdateNodeInfo("general", "General properties", "no-type");
-            WaterPressure = _device.CreateHostFloatProperty(PropertyType.State, "general", "actual-water-pressure", "Water pressure");
+            WaterPressure = _device.CreateHostNumberProperty(PropertyType.State, "general", "actual-water-pressure", "Water pressure");
 
             _device.UpdateNodeInfo("ambient", "Ambient properties", "no-type");
-            Pressure = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "pressure", "Pressure", 0, "hPa");
-            Temperature = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "temperature", "Temperature", 0, "°C");
-            Humidity = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "humidity", "Humidity", 0, "%");
-            QualityIndex = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "quality-index", "Quality index");
+            Pressure = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "pressure", "Pressure", 0, "hPa");
+            Temperature = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "temperature", "Temperature", 0, "°C");
+            Humidity = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "humidity", "Humidity", 0, "%");
+            QualityIndex = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "quality-index", "Quality index");
 
             _device.UpdateNodeInfo("system", "System", "no-type");
-            _systemUptime = _device.CreateHostFloatProperty(PropertyType.State, "system", "uptime", "Uptime", 0, "h");
-            _systemIpAddress = _device.CreateHostStringProperty(PropertyType.State, "system", "ip-address", "IP address", Program.GetLocalIpAddress());
+            _systemUptime = _device.CreateHostNumberProperty(PropertyType.State, "system", "uptime", "Uptime", 0, "h");
+            _systemIpAddress = _device.CreateHostTextProperty(PropertyType.State, "system", "ip-address", "IP address", Program.GetLocalIpAddress());
 
             Log.Info($"Initializing Homie entities.");
-            _broker.PublishReceived += _device.HandlePublishReceived;
-            _broker.Initialize(mqttBrokerIpAddress, _device.WillTopic, _device.WillPayload, (severity, message) => {
+            _broker.Initialize(mqttBrokerIpAddress, (severity, message) => {
                 if (severity == "Info") { Log.Info(message); }
                 else if (severity == "Error") { Log.Error(message); }
                 else { Log.Debug(message); }
             });
-            _device.Initialize(_broker.PublishToTopic, _broker.SubscribeToTopic);
+            _device.Initialize(_broker, (severity, message) => {
+                if (severity == "Info") { Log.Info(message); }
+                else if (severity == "Error") { Log.Error(message); }
+                else { Log.Debug(message); }
+            });
 
             new Thread(() => {
                 Log.Info($"Spinning up parameter monitoring task.");
